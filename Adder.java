33a2e272ba9aadac473fa2106be092a4fc8419a6
kittiwan <60050139@kmitl.ac.th>
2018-09-10 18:50:28 +0700

    add : adder.java

---
+++ Adder.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Adder {
+public static void main (String[] args){
+  Scanner sc = new Scanner(System.in);
+  System.out.print("Enter x=");
+  int x = sc.nextInt();
+  System.out.print("Enter y=");
+  int y = sc.nextInt();
+    System.out.println(x+y);
+  }
+}
+
